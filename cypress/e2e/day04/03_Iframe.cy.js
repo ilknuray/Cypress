9b2ff66d68a42e818a0575c73366ae58c0fab71d
ilknur yilmaz <ilknuray1318@gmail.com>
2023-05-01 13:36:06 +0200

    Cypress

--- cypress/e2e/day04/03_Iframe.cy.js
+++ cypress/e2e/day04/03_Iframe.cy.js
@@ -4,7 +4,12 @@ describe('Iframe tests',() => {
 
     it('Iframe',() => {
     cy.visit('https://the-internet.herokuapp.com/iframe')
-    
+    //1)plugin indirilir
+   // npm.js sitesinden
+    //2)plugin import edilerek support altindaki dosyaya yapistirilir
+
+
+
 
 
     })
