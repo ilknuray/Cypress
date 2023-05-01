b039c41fabdd7aa3b476abbbc52c89d0d3d57ac4
ilknur yilmaz <ilknuray1318@gmail.com>
2023-05-01 12:39:12 +0200

    Cypress

--- cypress/e2e/day04/02_CheckBoxes.cy.js
+++ cypress/e2e/day04/02_CheckBoxes.cy.js
@@ -1,23 +1,27 @@
 /// <reference types="cypress" />
 
-describe('',() => {
+describe('Check boxes',() => {
 
-    it('',() => {
+    it('TestCase 1',() => {
+    cy.visit('http://automationpractice.pl/index.php')
+    cy.get('.sf-menu > :nth-child(1) > [href="http://automationpractice.pl/index.php?id_category=3&controller=category"]').click()
+    //first()=eq(0) ilk elemente tiklar
+    cy.get('#layered_category_4').should('not.be.checked')
+    //not.be.checked tikli olmadigini kontrl eder.test eder
+    
+
+    })
+    it('Test Case 2',() => {
 
 
 
     })
-    it('',() => {
+    it('Test Case 3',() => {
 
 
 
     })
-    it('',() => {
-
-
-
-    })
-    it('',() => {
+    it('Test Case 4',() => {
 
 
 
