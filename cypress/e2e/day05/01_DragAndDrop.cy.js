e6d103c1946b20e914e4acc650c146e9604e4d50
ilknur yilmaz <ilknuray1318@gmail.com>
2023-05-01 14:43:38 +0200

    Cypress

--- cypress/e2e/day05/01_DragAndDrop.cy.js
+++ cypress/e2e/day05/01_DragAndDrop.cy.js
@@ -4,12 +4,29 @@ describe('',() => {
 
     it('',() => {
         cy.visit('https://the-internet.herokuapp.com/drag_and_drop')
-        cy.get('#column-a').wait(3000).drag('#column-b')
+        cy.get('#column-a').wait(3000).drag('#column-b').then((success) => {
+            assert.isTrue(success)
+        })
+
+
 
 
 
     })
-    it('',() => {
+    it.only('',() => {
+        cy.visit('https://the-internet.herokuapp.com/drag_and_drop')
+        const dataTransfer=new DataTransfer();
+        cy.wait(4000)
+        cy.get('#column-a').trigger('dragstart', {
+            dataTransfer
+            
+        })
+        cy.wait(6000)
+        cy.get('#column-b').trigger('drop', {
+            dataTransfer
+        })
+           
+        
 
 
 
