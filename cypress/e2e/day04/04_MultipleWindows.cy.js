1414071e01856ab42f6204ac3e608dbfc558e6f0
ilknur yilmaz <ilknuray1318@gmail.com>
2023-05-01 14:33:48 +0200

    Cypress

--- cypress/e2e/day04/04_MultipleWindows.cy.js
+++ cypress/e2e/day04/04_MultipleWindows.cy.js
@@ -1,8 +1,14 @@
 /// <reference types="cypress" />
 
-describe('',() => {
+describe('Multiple windows',() => {
 
-    it('',() => {
+    it('ikinci siteye gecis Rrmovind attribute',() => {
+        cy.visit('https://the-internet.herokuapp.com/windows')
+       // cy.get('.example > a').click()
+
+        cy.get('.example > a').invoke('removeAttr', 'target').click()
+        cy.get('h3').should('be.visible')
+
 
 
 
---
+++ cypress/e2e/day05/01_DragAndDrop.cy.js
@@ -0,0 +1,20 @@
+/// <reference types="cypress" />
+
+describe('',() => {
+
+    it('',() => {
+        cy.visit('https://the-internet.herokuapp.com/drag_and_drop')
+        cy.get('#column-a').wait(3000).drag('#column-b')
+
+
+
+    })
+    it('',() => {
+
+
+
+    })
+
+
+
+})
