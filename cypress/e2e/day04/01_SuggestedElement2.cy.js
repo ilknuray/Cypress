6d11dacc29d9588953ce275ccbf1cea83de303ec
ilknur yilmaz <ilknuray1318@gmail.com>
2023-05-01 12:25:24 +0200

    Cypress

--- cypress/e2e/day04/01_SuggestedElement2.cy.js
+++ cypress/e2e/day04/01_SuggestedElement2.cy.js
@@ -3,7 +3,10 @@
 describe('Suggested element 2',() => {
 
     it('Test Case',() => {
-
+    cy.visit('http://automationpractice.pl/index.php')
+    cy.get('#search_query_top').type('dress')
+    cy.wait(3000)
+    cy.get("li[class='ac_even ac_over']").click()
 
 
     })
