e82220a8fe361af1ae43b24f40c132686c91e3c0
ilknur yilmaz <ilknuray1318@gmail.com>
2023-05-01 13:54:00 +0200

    Cypress

--- cypress/e2e/day04/03_Iframe.cy.js
+++ cypress/e2e/day04/03_Iframe.cy.js
@@ -4,9 +4,21 @@ describe('Iframe tests',() => {
 
     it('Iframe',() => {
     cy.visit('https://the-internet.herokuapp.com/iframe')
-    //1)plugin indirilir
-   // npm.js sitesinden
-    //2)plugin import edilerek support altindaki dosyaya yapistirilir
+
+
+
+     /*
+      1)plugin indirilir
+      npm.js sitesinden
+      2)plugin import edilerek support altindaki dosyaya yapistirilir
+      3)Bu islmelerden sonra iframe tanima komutunu tanimlariz
+      cy.frameloaded ve cy.irame()
+      */
+      cy.frameLoaded('#mce_0_ifr')//iframe in yerini tanimla
+      //cy.iframe('#mce_0_ifr')
+      cy.iframe().find('p').clear()
+      cy.iframe().find('p').type('Cypress')
+      cy.get('.large-4 > div > a').should('contain.text','Elemental Selenium').click()
 
 
 
